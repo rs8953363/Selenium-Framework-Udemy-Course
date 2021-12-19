f80e4ed5eefdf90e8f803cec2fe2378d7920b4cf
rs8953365 <rs8953363@gmail.com>
2021-12-18 21:41:15 -0600

    ...

---
+++ src/main/java/com/Locators3ParentAndChild.java
@@ -0,0 +1,23 @@
+package com;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators3ParentAndChild {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium Dependencies\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+// Sibling - Child to parent traverse
+//header/div/button[1]/following-sibling::button[1]
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        Thread.sleep(2000);
+        System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+        System.out.println(driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).getText());
+    }
+}
+
+
+
