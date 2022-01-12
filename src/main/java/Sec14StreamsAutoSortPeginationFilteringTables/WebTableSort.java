76b769e802bb7a92714a5c0fbb92e0045ce68eae
Rose <rs8953363@gmail.com>
2022-01-12 00:43:06 -0600

    ...

--- src/main/java/Sec14StreamsAutoSortPeginationFilteringTables/WebTableSort.java
+++ src/main/java/Sec14StreamsAutoSortPeginationFilteringTables/WebTableSort.java
@@ -10,7 +10,7 @@ import java.util.stream.*;
 
 public class WebTableSort {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","C:\\browserdrivers\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\browserdrivers\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
@@ -19,7 +19,7 @@ public class WebTableSort {
         // capture all webelements into list
         List<WebElement> elementList = driver.findElements(By.xpath("//tr/td[1]"));
         // capture text of all webelements into new(original) list
-        List<String> originalList = elementList.stream().map(s->s.getText()).collect(Collectors.toList());
+        List<String> originalList = elementList.stream().map(s -> s.getText()).collect(Collectors.toList());
         // sort in the original list -->sorted list
         List<String> sortedList = originalList.stream().sorted().collect(Collectors.toList());
         // compare original list vs sorted list
@@ -27,15 +27,20 @@ public class WebTableSort {
 
         //Int Q: How to print each item on a web table? Scan the Veg/fruit name column with getText ->Beans->
         // print the price of rice
+        List<String> price;
+        do {
+            List<WebElement> rows = driver.findElements(By.xpath(("//tr/td[1]"));
+            price = rows.stream().filter(s -> s.getText().contains("Rice")).map(s -> getPriceVeggie(s))
+                    .collect(Collectors.toList());
 
-        List<String> price = elementList.stream().filter(s->s.getText().contains("Beans")).map(s->getPriceVeggie(s))
-                .collect(Collectors.toList());
-        price.forEach(a-> System.out.println(a));
-
+        price.forEach(a -> System.out.println(a));
+        if (price.size() < 1) {
+            driver.findElement(By.cssSelector("[aria-label='Next']")).click();//Clicks on the next button
+        }
+    }while(price.size()<1);
     }
     private static String getPriceVeggie(WebElement s){
-       String pricevalue =git add .
-                s.findElement(By.xpath("following-sibling::td[1]")).getText();
+       String pricevalue = s.findElement(By.xpath("following-sibling::td[1]")).getText();
         return pricevalue;
     }
 }
